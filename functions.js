d47aac6fefbc5e68c516eab7fade59047491c929
Jordan Parrott <parrottjrs@gmail.com>
2023-02-23 08:39:51 -0330

    more work
    Added /amended functions, variable names, and styles. Added notes.

--- functions.js
+++ functions.js
@@ -1,13 +1,42 @@
+// ******* ******* ****
+// creates landing page
+// ******* ******* ****
+
 function openHomePage() {
   h1.innerText = "Notes";
-  btn.innerText = "+ new note";
-  header.append(h1, btn);
+  moveButton.innerText = "+ new note";
+  header.append(h1, moveButton);
+  moveButton.addEventListener("click", () => {
+    newNote();
+  });
 }
 
+// ******** ******* **** ******** **** **** ******** ********
+// replaces landing page elements with note creation elements
+// ******** ******* **** ******** **** **** ******** ********
+
 function newNote() {
-  header.removeChild(h1);
-  btn.innerText = "back";
-  header.prepend(title);
-  title.setAttribute("placeholder", "My New Note...");
-  title.setAttribute("class", "title");
+  h1.remove();
+  moveButton.innerText = "back";
+  header.prepend(titleInput);
+  container.append(noteText, saveButton);
+  moveButton.addEventListener("click", () => {
+    backToHome();
+  });
+}
+
+// ******* ** ******* **** **** *** **** ***** *** ****
+// returns to landing page with new note added (if any)
+// ******* ** ******* **** **** *** **** ***** *** ****
+
+function backToHome() {
+  titleInput.remove();
+  noteText.remove();
+  saveButton.remove();
+  h1.innerText = "Notes";
+  moveButton.innerText = "+ new note";
+  header.prepend(h1);
+  moveButton.addEventListener("click", () => {
+    newNote();
+  });
 }
--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+// vars
 const header = document.querySelector("#header");
+const container = document.querySelector("#container");
 let h1 = document.createElement("h1");
-let btn = document.createElement("button");
-let title = document.createElement("input");
+let moveButton = document.createElement("button");
+let saveButton = document.createElement("button");
+let titleInput = document.createElement("input");
+let note = document.createElement("form");
+let noteText = document.createElement("textarea");
+
+// attributes
+moveButton.classList.add("move-button");
+
+saveButton.classList.add("save-button");
+saveButton.innerText = "save note";
+
+titleInput.setAttribute("type", "text");
+titleInput.setAttribute("placeholder", "My New Note...");
+titleInput.classList.add("title-input");
+noteText.setAttribute("rows", "30");
+noteText.setAttribute("cols", "100");
+noteText.setAttribute(
+  "placeholder",
+  "The most important thing I need to do is..."
+);
